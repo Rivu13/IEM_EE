37850ae77e6f78cf510b53506db6a6a7c4253809
Abhishek Mukherjee <abhishekmukherjeebhai789@gmail.com>
2026-07-10 15:21:20 +0530

    Refactor code structure for improved readability and maintainability

--- src/data/outreach.js
+++ src/data/outreach.js
@@ -9,38 +9,43 @@ export const outreachProgrammes = [
   {
     images: ['out5.png', 'out6.png', 'out7.png', 'out8.png', 'out9.png'],
     paragraphs: [
-      "The Board of Practical Training (Eastern Region) [BOPT (ER)] organized an Apprenticeship Employers' Development Program (AEDP) Seminar on 31st October 2025 at the Biswa Bangla Convention Centre, Kolkata. The seminar aimed to promote awareness about the National Apprenticeship Training Scheme (NATS) and to strengthen collaboration between industries, academic institutions, and government agencies in enhancing employability through skill-based training.",
+      "The Board of Practical Training (Eastern Region) [BOPT (ER) organized an Apprenticeship Employers’ Development Program (AEDP) Seminar on 31st October 2025 at the Biswa Bangla Convention Centre, Kolkata. The seminar aimed to promote awareness about the National Apprenticeship Training Scheme (NATS) and to strengthen collaboration between industries, academic institutions, and government agencies in enhancing employability through skill-based training.",
       'The main objective of the seminar was to highlight the importance of apprenticeship training as a bridge between education and employment. It focused on sensitizing employers, academic administrators, and students to the benefits of structured apprenticeship programs under the Ministry of Education, Government of India. The seminar also discussed strategies for effective implementation of NATS and the role of higher educational institutions in facilitating industry-ready skill development.',
     ],
   },
   {
     images: ['out10.png', 'out11.png', 'out12.png', 'out13.png'],
     paragraphs: [
-      'In line with the vision to bridge the gap between academia and industry, Dr. Sourav Das successfully completed the 6-day Faculty Development Training on 18th – 24th August under the MG Nurture Program – CAEV (Connected, Autonomous, and Electric Vehicles) combined with AI module, organized by JSW MG Motor India Pvt. Ltd. in collaboration with Skill-Lync.',
+      'In line with the vision to bridge the gap between academia and industry, Dr. Sourav Das successfully completed the 6-day Faculty Development Training on 18th - 24th August under the MG Nurture Program – CAEV (Connected, Autonomous, and Electric Vehicles) combined with AI module, organized by JSW MG Motor India Pvt. Ltd. in collaboration with Skill-Lync. ',
     ],
   },
   {
-    images: ['out14.png', 'out15.png', 'out16.png'],
-    paragraphs: ['Outreach programme details to be added.'],
+    images: ['out14.png', 'out15.png', 'out16.png', 'out17.png'],
+    paragraphs: ['The Confederation of Indian Industry (CII), Eastern Region, successfully organized a two-day conference on "Catalyzing Growth: Focusing on Sustainability, Technology, and Innovation" at Raj Kutir,Swabhumi Kolkata, on 18th and 19th March 2025. The event brought together industry leaders, policymakers, academics, and innovators to discuss strategies for sustainable growth while leveraging technology and fostering innovation. ',
+
+      'Dr. Unmesha Ray, from the Department of Electrical Engineering, had the privilege to attend the conference on behalf of the department.'
+    ],
   },
   {
-    images: ['out17.png', 'out18.png', 'out19.png'],
-    paragraphs: ['Outreach programme details to be added.'],
+    images: ['out18.png'],
+    paragraphs: ['Dr. Unmesha Roy presented work at the International Conference in Sustainable Batteries (ICSB-25), held at Biswa Bangla Convention Centre, Kolkata from February 24-27, 2025. This conference was organized by TCG CREST, which aimed to facilitate in-depth discussions on the latest advancements in various battery chemistries, including lithium (Li), sodium (Na), solid-state, and aqueous batteries. ICSB-25 also offered an excellent opportunity for networking with numerous Indian and international companies. Participants connected with organizations such as Exide Energy, KPIT Tech, UNIGRID, Rincell, Ola Electric, JSW, Mahindra, Amaraja, Loham, Tata Chemicals, Tata Steel, Log9, Gujarat Fluorochemicals, QuantumScape, Reliance, Lyten, Himadri Chemicals, Factorial Energy, and many others.'],
   },
   {
-    images: ['out20.png', 'out21.png', 'out22.png'],
-    paragraphs: ['Outreach programme details to be added.'],
+    images: ['out19.png','out20.png', 'out21.png', 'out22.png'],
+    paragraphs: ['IEEE Kolkata Section in association with Mathworks organised a tech seminar titled  AI and IoT Empowering Academia and Industry on 6th March 2025, 9 am to 4: 30 pm  at The Westin Kolkata Rajarhat.',
+                  'Dr. Sourav Das along with Dr. Unmesha Ray from department of electrical engineering have attended this seminar. '
+    ],
   },
   {
-    images: ['out23.png', 'out24.png', 'out25.png'],
-    paragraphs: ['Outreach programme details to be added.'],
+    images: ['out23.png'],
+    paragraphs: ['Prof. Unmesha Ray, EE Department had speaking at the Cloud@DevFest hosted by Google Developer Group GDG Cloud Kolkata on December 22, 2024. The event, held at the Banquet Hall, Offbeat CCU, drew an impressive crowd of over 200 attendees. She thoroughly enjoyed engaging with a dynamic community of talented engineers and tech enthusiasts, each excelling in their respective fields.'],
   },
   {
-    images: ['out26.png'],
-    paragraphs: ['Outreach programme details to be added.'],
+    images: ['out24.png'],
+    paragraphs: ['The Conference on Science of Consciousness was jointly organized by Bhaktivedanta Institute,Kolkata and Bharatiya Vidya Mandir, Kolkata at Jadavpur University from 10th to 11th August 2024. The conference aimed to foster a holistic approach among the participants for exploring consciousness in a multidisciplinary way. A group of students along with a few faculty members (Prof. Unmesha Ray, EE Department and Prof. Suman Sarkar, EEE Department) from IEM, Saltlake attended this program.'],
   },
   {
-    images: ['out27.png'],
-    paragraphs: ['Outreach programme details to be added.'],
+    images: ['out25.png'],
+    paragraphs: ["Dr. Sourav Das has attended Educator's Conclave organized by CII on 13.07.24 at Taj Raajkuthir where IEM, Kolkata was honored with Acharya Samman Award by CII."],
   },
 ]
