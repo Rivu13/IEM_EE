232d5a40ddfe9bded40ea2e80874e898631e2af7
Argha Dalal <arghadalal354@gmail.com>
2026-07-14 12:38:50 +0530

    Improvement: The images parts.

--- src/data/industrialTour.js
+++ src/data/industrialTour.js
@@ -1,59 +1,202 @@
-import { imageRange } from '../utils/imageRange'
+import { imageRange } from "../utils/imageRange";
 
 export const industrialTours = [
   {
-    id: 'wbreda',
-    title: 'West Bengal Renewable Energy Development Agency (WBREDA)',
-    date: '16.01.2026',
-    coordinator: 'Prof. Debasis Jana',
-    images: imageRange('it', 1, 4),
+    id: "wbreda",
+    title: "West Bengal Renewable Energy Development Agency (WBREDA)",
+    date: "16.01.2026",
+    coordinator: "Prof. Debasis Jana",
+    images: imageRange("it", 1, 4),
     paragraphs: [
-      "On 16th January, 2026, the IEI - IEM Students' Chapter (EE), Department of Electrical Engineering, IEM Kolkata, successfully organized an educational industry visit to the West Bengal Renewable Energy Development Agency (WBREDA), Bidyut Shakti Bhawan, Sector V, Kolkata. The visit was aimed at providing students with industry-oriented exposure to the design, deployment, and operational aspects of solar power plants.",
-      "The interaction offered students a valuable opportunity to gain practical insights into modern solar energy technologies and their real-world applications. Experts from WBREDA shared their extensive experience in renewable energy projects and highlighted the growing importance of solar power in meeting sustainable energy demands. The session helped students understand how theoretical concepts learned in classrooms are implemented at the industry level, particularly in large-scale solar installations.",
+      "On 16th January, 2026, the IEI–IEM Students’ Chapter (EE), Department of Electrical Engineering, IEM Kolkata, successfully organized an educational industry visit to the West Bengal Renewable Energy Development Agency (WBREDA), Bikalpa Shakti Bhavan, Sector V, Kolkata. The visit was aimed at providing students with industry-oriented exposure to the design, deployment, and operational aspects of solar power plants.",
+      "The interaction offered students a valuable opportunity to gain practical insights into modernsolar energy technologies and their real-world applications. Experts from WBREDA sharedtheir extensive experience in renewable energy projects and highlighted the growing importance of solar power in meeting sustainable energy demands. The session helped students understand how theoretical concepts learned in classrooms are implemented at the industry level, particularly in large-scale solar installations.",
     ],
   },
   {
-    id: 'mg-motor',
-    title: 'MG Motor India Ltd',
-    date: '13.01.2026',
-    coordinator: 'Prof. (Dr.) Sourav Das, Prof. (Dr.) Unmesha Ray',
-    images: imageRange('it', 5, 3),
+    id: "mg-motor",
+    title: "MG Motor India Ltd",
+    date: "13.01.2026",
+    coordinator: "Prof. (Dr.) Sourav Das, Prof. (Dr.) Unmesha Ray",
+    images: imageRange("it", 5, 3),
     paragraphs: [
-      'An industrial visit was organized by the IEEE IAS IEM Student Branch Chapter in association with the IEEE IES IEM Student Branch Chapter to MG Motor India Ltd, Halol, Gujarat, on 13th January 2026. The visit was planned to provide students with first-hand exposure to the automobile manufacturing industry and to help them understand modern vehicle production techniques, assembly line operations, and industrial management practices.',
-      '07 students from the Electrical Engineering Department, Institute of Engineering and Management (IEM), Kolkata, attended the industrial visit.',
+      "An industrial visit was organized by the IEEE IAS IEM Student Branch Chapter in association with the IEEE IES IEM Student Branch Chapter to MG Motor India Ltd., Halol, Gujarat, on 13th January 2026. The visit was planned to provide students with first-hand exposure to the automobile manufacturing industry and to help them understand modern vehicle production techniques, assembly line operations, and industrial management practices.",
+      "07 students from the Electrical Engineering Departments, Institute of Engineering and Management (IEM), Kolkata, attended the industrial .",
     ],
   },
   {
-    id: 'polycab',
-    title: 'POLYCAB INDIA LTD.',
-    date: '21.08.2025',
-    coordinator: 'Prof. (Dr) Sourav Das',
-    images: imageRange('it', 8, 3),
+    id: "polycab",
+    title: "POLYCAB INDIA LTD.",
+    date: "21.08.2025",
+    coordinator: "Prof. (Dr) Sourav Das",
+    images: imageRange("it", 8, 1),
     paragraphs: [
-      'As part of the ongoing collaboration between the Department of Electrical Engineering, IEM Kolkata, and Polycab India Ltd, Halol, Gujarat, an industry visit was conducted on 21st August 2025. This collaboration stems from the Memorandum of Understanding (MoU) signed in July 2025, which remains valid for three years.',
-      'The primary objective of this visit was to strengthen the academia-industry linkage through discussions on student internships, placement opportunities, and consultancy projects.',
+      "As part of the ongoing collaboration between the Department of Electrical Engineering, IEM Kolkata, and Polycab India Ltd., Halol, Gujarat, an industry visit was conducted on 21st August 2025. This collaboration stems from the Memorandum of Understanding (MoU) signed in July 2023, which remains valid for three years.",
+      "The primary objective of this visit was to strengthen the academia-industry linkage through discussions on student internships, placement opportunities, and consultancy projects.",
+    ],
+  },
+  // 👇👇👇
+  {
+    id: "polycab",
+    title: "Durgapur Projects Limited (DPL)",
+    date: "19.05.2025 - 18.06.2025",
+    coordinator: "Prof. (Dr.) Deepro Sen",
+    images: imageRange("it", 9, 4),
+    paragraphs: [
+      "The vocational training at DPL provided invaluable insights into thermal power generation, bridging the gap between theoretical knowledge and practical application. Students learned about plant operations, equipment functionality, and the importance of safety and efficiency in power systems.",
+      "The experience underscored the critical role of thermal power plants in meeting energy demands while highlighting the need for sustainable practices. The training was a significant step toward preparing future engineers for the challenges of the power sector.",
+      "07 students from the Electrical Engineering Departments, Institute of Engineering and Management (IEM), Kolkata, attended the vocational training.",
     ],
   },
   {
-    id: 'polycab',
-    title: 'POLYCAB INDIA LTD.',
-    date: '21.08.2025',
-    coordinator: 'Prof. (Dr) Sourav Das',
-    images: imageRange('it', 8, 3),
+    id: "polycab",
+    title: "TATA POWER SKILL DEVELOPLEMENT INSTITUTE",
+    date: " 12.05.2025 - 29.05.2025",
+    coordinator: "Prof. (Dr.) Sourav Das",
+    images: imageRange("it", 13, 2),
     paragraphs: [
-      'As part of the ongoing collaboration between the Department of Electrical Engineering, IEM Kolkata, and Polycab India Ltd, Halol, Gujarat, an industry visit was conducted on 21st August 2025. This collaboration stems from the Memorandum of Understanding (MoU) signed in July 2025, which remains valid for three years.',
-      'The primary objective of this visit was to strengthen the academia-industry linkage through discussions on student internships, placement opportunities, and consultancy projects.',
+      "The IEEE IAS IEM Students Branch Chapter is proud to highlight the participation of our members in a specialized Skill Development Training Program conducted by Tata Power Skill Development Institute (TPSDI). This initiative was taken by IEEE-IAS (INDUSTRY APPLICATION SOCIETY) IEM Students Branch Chapter & IEEE-IES (INDUSTRY ELECTRONICS SOCIETY) IEM SBC aiming at equipping students with practical industryrelevant skills aligned with the evolving needs of the power and energy sector.",
     ],
   },
   {
-    id: 'polycab',
-    title: 'POLYCAB INDIA LTD.',
-    date: '21.08.2025',
-    coordinator: 'Prof. (Dr) Sourav Das',
-    images: imageRange('it', 8, 3),
+    id: "polycab",
+    title: "Purulia Pumped Storage Project",
+    date: "03.03.2025",
+    coordinator: "Prof. (Dr) Sourav Das",
+    images: imageRange("it", 15, 3),
     paragraphs: [
-      'As part of the ongoing collaboration between the Department of Electrical Engineering, IEM Kolkata, and Polycab India Ltd, Halol, Gujarat, an industry visit was conducted on 21st August 2025. This collaboration stems from the Memorandum of Understanding (MoU) signed in July 2025, which remains valid for three years.',
-      'The primary objective of this visit was to strengthen the academia-industry linkage through discussions on student internships, placement opportunities, and consultancy projects.',
+      "The industrial visit to the Purulia Pumped Storage Project (PPSP), Purulia, was aimed at providing students with practical exposure to advanced hydroelectric power generation and energy storage systems. The visit allowed participants to gain valuable insights into the real-world working of a large-scale pumped storage hydroelectric plant.",
+      "16 students from the Electrical and Electronics & Communication Engineering Departments, Institute of Engineering and Management (IEM), Kolkata, attended the industrial visit. ",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "Haldia Energy Limited",
+    date: "29.03.2025",
+    coordinator: "Prof. Ankit Ray Ghatak",
+    images: imageRange("it", 18, 6),
+    paragraphs: [
+      " On March 29, 2025, 𝗜𝗘𝗧 𝗞𝗟𝗡  in association with 𝗘𝗘 &  𝗘𝗘𝗘 Department,  𝗜𝗘𝗠 𝗞𝗼𝗹𝗸𝗮𝘁𝗮 organized an one-day industry visit to 𝗛𝗮𝗹𝗱𝗶𝗮 𝗘𝗻𝗲𝗿𝗴𝘆 𝗟𝗶𝗺𝗶𝘁𝗲𝗱 providing students with valuable real-world exposure. Total 28 students from various college including 𝗳𝗶𝘃𝗲 𝗮𝗰𝘁𝗶𝘃𝗲 𝘀𝘁𝘂𝗱𝗲𝗻𝘁𝘀 𝗳𝗿𝗼𝗺 𝗜𝗘𝗧-𝗜𝗘𝗠 𝗢𝗻 𝗖𝗮𝗺𝗽𝘂𝘀  𝗦𝘁𝘂𝗱𝗲𝗻𝘁 𝗖𝗵𝗮𝗽𝘁𝗲𝗿 attended this visit, gaining their hands-on experience with thermal plant's operations. During this visit, students explored various section of the power plant, including turbine section, boiler unit, control room, coal station etc. It was a highly beneficial visit, providing students with a practical understanding of thermal power generation, efficiency optimization, and sustainability challenges. It inspired them to align their academic knowledge with industry needs and explore career opportunities in the energy sector.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "TATA POWER SKILL DEVELOPLEMENT INSTITUTE",
+    date: "03.01.2025 - 24.01.2025",
+    coordinator: "Prof. (Dr.) Sourav Das",
+    images: imageRange("it", 24, 10),
+    paragraphs: [
+      "The training program at Tata Power Skill Development Institute (TPSDI), Maithon, was designed to provide students with a comprehensive understanding of advanced industrial skills required for engineers. The program covered basic, moderate, and advanced levels of training, ensuring a holistic learning experience. The curriculum was meticulously structured to combine theoretical knowledge with practical, hands-on experience, preparing students for real-world industrial challenges. Around 30 students from the Batch of 2021, 2022 and 2023 of IEM Kolkata got an opportunity to be a part of this training program.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "Maithon Thermal Power Plant",
+    date: "29.08.2024",
+    coordinator: ["Prof. Ankit Ray Ghatak, ", "Prof. (Dr.) Sourav Das"],
+    images: imageRange("it", 34, 5),
+    paragraphs: [
+      "Glimpses of the Industry visit to Maithon Thermal Power organized by Department of EE & EEE in association with IET(UK) Kolkata Local Network.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "JADAVPUR UNIVERSITY RESEARCH LAB.",
+    date: "20.08.2024",
+    coordinator: "Prof. Unmesha Ray",
+    images: imageRange("it", 39, 4),
+    paragraphs: [
+      "Around 15 students from the Batch of 2022 and 2023 of IEM Kolkata got an opportunity to be a part of the one-day ‘RESEARCH LAB VISIT’ at School of Materials Science & Nanotechnology, Jadavpur University, organized by Department of Electrical Engineering, Institute of Engineering & Management. This lab visit was a part of a Scientific Social Responsibility activity of SERB (SERB-ANRF) sanctioned project entitled “Hetero atom doped Carbon quantum dot/nanoparticle based security ink for inkjet printed anti-counterfeiting and fingerprint verification applications with neural network based authentication” (CRG/2021/002823). The main aim of this event was to support the youth towards creating new research projects using nanotechnology and gather vast knowledge about nanotechnology and material science.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "Tractors India Limited",
+    date: "19.07.2024",
+    coordinator: ["Prof. (Dr.) Sourav Das, ", "Prof. Ankit Ray Ghatak"],
+    images: imageRange("it", 43, 5),
+    paragraphs: [
+      "The industry visit to Tractors India Limited was a highly educational and enriching experience for the students and faculty of the Institute of Engineering and Management, Kolkata. It successfully provided a platform for the students to connect their academic knowledge with practical industry applications, thereby enhancing their understanding and inspiring them to pursue excellence in their respective fields.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "Mejia Thermal Power Station (MTPS), DVC.",
+    date: "24.03.2023",
+    coordinator: "Prof. Ankit Ray Ghatak",
+    images: imageRange("it", 48, 4),
+    paragraphs: [
+      "The students were guided to the basic knowledge of coal based Thermal Power Plants and provided with the safety helmet. Training supervisor from MTPS accompanied students’ group and showed them control room, turbine room, generator room and switchyard of MTPS.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "Eastern Transformer & Equipment Pvt. Ltd.",
+    date: "27.04.2022 - 28.04.2022 ",
+    coordinator: "Prof. Ankit Ray Ghatak",
+    images: imageRange("it", 52, 5),
+    paragraphs: [
+      "All the 30 Student Members of IET-IEM Student Branch has well participated and secured knowledge on Transformer construction techniques and procedure involved in transformer construction. Various methods of testing the Transformers are known and methods are noted. Information on testing the transformers is encapsulated.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "National Test House (NTH), ER",
+    date: "14.03.2022 & 17.03.2022",
+    coordinator: "Prof. Debashis Jana",
+    images: imageRange("it", 57, 8),
+    paragraphs: [
+      "Students along with their faculty member from Electrical Engineering department, IEM Kolkata have visited NTH, Salt Lake, Kolkata for two days technical workshop. The program was started with the inauguration speech delivered by Director, NTH, Kolkata followed by other expert members. Then, laboratory visit was done splitting the total students into two groups. One group attended the instrumentation & calibration laboratory and another group attended Lamp & Photometry laboratory.",
+    ],
+  },
+  {
+    id: "polycab",
+    title:
+      "MEJIA THERMAL POWER PLANT (MTPS) under DAMODAR VALLEY CORPORATION (DVC)",
+    date: "28.02.2020",
+    coordinator: "Prof. Ankit Ray Ghatak",
+    images: imageRange("it", 65, 5),
+    paragraphs: [
+      "We had organized a one day industrial visit for the EE AND EEE 3rd year student (attendance was 50) in MEJIA THERMAL POWER PLANT (MTPS) under DAMODAR VALLEY CORPORATION (DVC). Students where very much excited after the visit, they get to know about the different control strategies applied in the thermal power plant. Also able to see the working operation of the different alternator, transformer, motors and gathered lots of practical knowledge from the industry experts.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "Industry Visit to WBSEDCL Substation, Sector-V",
+    date: "09.11.2019",
+    coordinator: "Prof. Yashaswy Raj",
+    images: imageRange("it", 70, 2),
+    paragraphs: [
+      "We had organized a one day industrial visit for the EE AND EEE 3rd year student (attendance was 50) in WBSEDCL Substation, Sector-V. Students where very much excited after the visit, they get to know about the different control strategies applied in the thermal power plant. Also able to see the working operation of the different alternator, transformer, motors and gathered lots of practical knowledge from the industry experts.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "Industrial visit at Tata Steel Plant, Jamshedpur",
+    date: "23.05.2016",
+    coordinator: "Prof.(Dr.) Arun Kr. Bar, Prof. Somnath Hazra",
+    images: imageRange("it", 72, 5),
+    paragraphs: [
+      "On 23rd May 2016, 14 students of Electrical Engg. & Mechanical Engg. Department accompanied by college faculties Prof (Dr.) Arun Kumar Bar & Prof. Somanth Hazra went for an one day Industrial visit to the Tata Steel Plant (formerly TISCO) in Jamshedpur organised by CII . It was the 10th largest steel producing plant in the world in 2015 and is currently 2nd largest in India after SAIL.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "Industrial Visit to Tractors India Limited, Kharagpur",
+    date: "15.02.2018",
+    coordinator: "Prof. Debashis Jana, Prof. Ankit Ray Ghatak",
+    images: imageRange("it", 77, 5),
+    paragraphs: [
+      "Visit started off  by seeing the different machinery involved like Grove cranes, Reach stackers, Containers handler, Lorry loaders all representing the global technology. Milling, grinding and the entire process of fabrication, gushing jet of plasma cut hard metallic sheets did give the students all chills. The electrical division from the switch gear room to the transformers installed did gain outright knowledge concerning the entire domain including insulation and frequent use of circuit breakers throughout the plant.",
+    ],
+  },
+  {
+    id: "polycab",
+    title: "Industrial visit to Himadri Chemicals",
+    date: "25.08.2016",
+    coordinator: "Prof. Nirban Kumar Saha",
+    images: imageRange("it", 82, 7),
+    paragraphs: [
+      "The objective of the visit is to help both the students and faculty to provide an opportunity for the faculty members of engineering institutions to align their academic experiences to hands on real world processes. With a view to improve the employability efficiency of the students graduating from the Engineering institutions",
     ],
   },
   // ...Array.from({ length: 12 }, (_, i) => {
@@ -67,4 +210,7 @@ export const industrialTours = [
   //     paragraphs: ['Add industrial tour details here.'],
   //   }
   // }),
-]
+];
+
+
+// Check all above "ID' after the "TITLE: POLYCAB INDIA LTD.", and change the ID after this title.
